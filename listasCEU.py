33169241143031854c4c15127ca805eaddd42c5d
criissuarezzz <cristina.suarezlimon@usp.ceu.es>
2022-10-27 12:28:32 +0200

    commit

--- listasCEU.py
+++ listasCEU.py
@@ -1,9 +1,4 @@
-#En este ejercicio se os pide crear un fichero llamado listasCEU.py que realice las siguientes funcionnes
-#Define una lista que contenga al menos 4 elementos de todos los tipos de valores permitidos en Python.
-#Selecciona cada dos elementos (uno si otro no) desde el final de la lista.
-#Cambia solamente la posición del primer elemento con el último elemento de la lista.
-#Elimina el último elemento de la lista modificada en el paso anterior.
-#Crea una nueva lista con la repetición de los elemento de la lista guardada en el paso anterior.
+#https://github.com/criissuarezzz/parcial27-10.git
 valor1=int(input("Introduzca un número entero:"))
 valor2=float(input("Introduzca un número decimal:"))
 valor3=str(input("Introduzca una palabra:"))
